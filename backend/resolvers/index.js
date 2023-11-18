bb15560f6d39bd1d64f50af152608a017d37b965
GregFJr <finney101690@gmail.com>
2023-11-18 14:24:38 -0700

    updated resolvers and added sigin and signup mutations

--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const Message = require('../models/Message');
 const User = require('../models/User');
 const Game = require('../models/Game');
@@ -17,7 +19,6 @@ const resolvers = {
     users: async () => {
       return User.find({});
     },
-    
   },
   Mutation: {
     postMessage: async (_, { content, user, chatRoomId, gameId }) => {
@@ -38,22 +39,42 @@ const resolvers = {
       await game.save();
       return game;
     },
+    signUp: async (_, { username, email, password }) => {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        throw new Error('User already exists with that email');
+      }
 
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ username, email, password: hashedPassword });
+      await user.save();
+
+      return jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    },
+    signIn: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        throw new Error('Invalid password');
+      }
+
+      return jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    },
   },
   Subscription: {
     messagePosted: {
       subscribe: () => pubsub.asyncIterator([MESSAGE_POSTED]),
     },
-    
   },
-  
   Message: {
     game: async (message) => {
-      
       return message.gameId ? Game.findById(message.gameId) : null;
     }
   },
-
 };
 
 module.exports = resolvers;
